c842843a8d88bfe305f159401b364b106a891bfb
AlekseyBliznyuk <58771284+AlekseyBliznyuk@users.noreply.github.com>
2021-10-14 09:21:40 +0300

    Update Source.cpp

--- Task1/Source.cpp
+++ Task1/Source.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-double Fact(int n)
+double Fact(double x)
 {
-	if (n == 0)
-		return 1;
-	return n * Fact(n - 1);
+    if (x == 0)
+        return 1;
+    return x * Fact(x - 1);
 }
-double Sum(int n)
+double fsm(int n)
 {
-	double sum = 1;
-	for (int i = 1; i < n; i++)
-	{
-		sum += 1 / Fact(n);
-		return sum;
-	}
-}
+    double i = 1;
+    double sum = 0;
 
+    while (i <= n)
+    {
+        sum += 1 / (Fact(i));
+        i++;
+    }
+    return sum;
+}
 int main()
 {
-	int n;
-	cout << "n: ";
-	cin >> n;
-	cout << "Summa: " << Sum(n);
-	return 0;
+    int n;
+    cout << "n:"; cin >> n;
+    cout << "Summa: " << fsm(n);
+    return 0;
 }
